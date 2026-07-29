6634f297b45360c35abfadfd5192798aa3e15251
dren314 <jpeacockcrunchfit@gmail.com>
2026-07-29 14:33:46 -0400

    Added code for navbar distrubution for all navbar tags
    the link can be null and it wont run the rest of the code

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const link = document.getElementById("special-button");
-const today = new Date();
-const day = today.getDay();
-if (day == 0 || day == 3) {
-    link.href = "https://www.youtube.com/watch?v=hSYGWGFl-W8";
+//Dynamic pray the rosary buttontah
+const link = document.getElementById("rosary-button");
+
+if (link) {
+    const today = new Date();
+    const day = today.getDay();
+    if (day == 0 || day == 3) {
+        link.href = "https://www.youtube.com/watch?v=hSYGWGFl-W8";
+    }
+    else if (day == 1 || day == 6) {
+        link.href = "https://www.youtube.com/watch?v=gdZuweae75M";
+    }
+    else if  (day == 2 || day == 5) {
+        link.href ="https://www.youtube.com/watch?v=fucgiDu-Fd0";
+    }
+    else {
+        link.href = "https://www.youtube.com/watch?v=11dVyWAm_GM";
+    }
 }
-else if (day == 1 || day == 6) {
-    link.href = "https://www.youtube.com/watch?v=gdZuweae75M";
-}
-else if  (day == 2 || day == 5) {
-    link.href ="https://www.youtube.com/watch?v=fucgiDu-Fd0";
-}
-else {
-    link.href = "https://www.youtube.com/watch?v=11dVyWAm_GM";
-}
+// Fills in Navbar placeholder
+fetch("navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  });
